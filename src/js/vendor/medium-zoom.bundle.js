e06318565616a934b7ee6076a856b2ec4c6ea633
Ewan Edwards <eedwards@journaltech.com>
2023-06-07 15:46:11 -0700

    Adjust medium-zoom config so `nozoom` class is honored.

--- src/js/vendor/medium-zoom.bundle.js
+++ src/js/vendor/medium-zoom.bundle.js
@@ -2,7 +2,7 @@
   var Mzm = (window.mzm = require('medium-zoom/dist/medium-zoom.min'))
   Mzm(
     // 'span.frame img, span.zoom img, div.imageblock img',
-    'div.imageblock img, span img, div.imageblock:not(.nozoom) img, span:not(.nozoom) img',
+    'div.imageblock:not(.nozoom) img, span:not(.nozoom) img',
     {
       background: '#fff',
     }
