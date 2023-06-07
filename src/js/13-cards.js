9d7224b70562712b87fa44b23268afeeec0b345d
Ewan Edwards <eedwards@journaltech.com>
2023-06-07 15:45:13 -0700

    Add design and icons for a "cards" grid that can be authored in Asciidoc and presents nicely.

---
+++ src/js/13-cards.js
@@ -0,0 +1,34 @@
+;(function () {
+  'use strict'
+
+  const cardsBlocks = document.querySelectorAll('.cards')
+  if (!cardsBlocks) return
+
+  // Finds an ancestor node with matching class, or returns null
+  const findAncestor = (el, cls) => {
+    if (el.classList.contains(cls)) return el
+    while ((el = el.parentElement) && !el.classList.contains(cls)) {
+    }
+    return el
+  }
+
+  cardsBlocks.forEach((cards) => {
+    cards.querySelectorAll('.card').forEach((card) => {
+      console.log('Card:', card)
+      if (card.querySelector('.icon')) {
+        card.classList.add('hasIcon')
+      }
+
+      card.addEventListener('click', (e) => {
+        e.preventDefault()
+        const parent = findAncestor(e.target, 'card')
+        if (!parent) return
+
+        const link = parent.querySelector('.link a')
+        if (!link) return
+
+        window.location = link.getAttribute('href')
+      })
+    })
+  })
+})()
