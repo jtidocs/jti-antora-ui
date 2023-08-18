739ccbe075ce408276dde71ddf4d56516e3aa430
Ewan Edwards <eedwards@journaltech.com>
2023-08-18 13:04:48 -0700

    Use the `en-CA` locale for the page build time.

--- src/helpers/buildTime.js
+++ src/helpers/buildTime.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const now = new Date()
-const date = now.toLocaleDateString('iso')
+const date = now.toLocaleDateString('en-CA')
 const time = now.toLocaleTimeString('en-US', { timeZone: 'America/Vancouver', timeStyle: 'long' })
 const report = `${date} ${time}`
 
