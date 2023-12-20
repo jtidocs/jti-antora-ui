e2c19dad883d0c600b8443e4227f5db614983fed
Ewan Edwards <eedwards@journaltech.com>
2023-12-20 15:04:18 -0800

    For PDFs with component versions matching eSeries releases, include the version string in the PDF filename.

--- src/helpers/pdfLink.js
+++ src/helpers/pdfLink.js
@@ -30,13 +30,26 @@ module.exports = (ctx) => {
   const component = componentVersion.name
   if (component === 'ROOT') return false
 
-  const version = componentVersion.version
-  const filename = componentVersion.title
+  let version = componentVersion.version
+  let filename = componentVersion.title
     .replace(/[&:]/g, '')
     .replace(/[ ,/"']/g, '-')
     .replace(/--/g, '-')
     .toLowerCase() +
     '.pdf'
+
+  let fVersion = ''
+  if (version.match(/\d{4}\.\d{2}(-LTS)?/)) {
+    fTitle = componentVersion.title
+      .replace(/&.+?;|[^ \p{Alpha}0-9_\-.]/gu, '')
+      .replace(/[ _.]/g, '_')
+      .replace(/--+/g, '-')
+    fVersion = version
+      .replace(/&.+?;|[^ \p{Alpha}0-9_\-.]/gu, '')
+      .replace(/[ _.]/g, '_')
+      .replace(/--+/g, '-')
+    filename = `${fTitle}${fVersion}.pdf`
+  }
 
   const url = `${component}/${version}/${filename}`
   return url
