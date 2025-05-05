3145a0aa5d653559e3d7e91fead6b3d3982c059f
Ewan Edwards <eedwards@journaltech.com>
2025-05-05 09:50:27 -0700

    Fixes to let the PrismJS plugins line-numbers and line-highlight work.
    NOTE: Do not use these plugins together; their padding/margins conflict.

--- src/js/vendor/prism.bundle.js
+++ src/js/vendor/prism.bundle.js
@@ -1,5 +1,20 @@
 ;(function () {
-  var Prism = require('prismjs')
+  const hlLines = document.querySelectorAll('.doc div[class*=" lines-"].listingblock')
+  for (const d of hlLines) {
+    const classes = d.classList
+    for (const c of classes) {
+      const mg = c.match(/lines-(.+)/)
+      if (mg) {
+        const lines = mg[1].replace('_', ',')
+        const p = d.querySelector('pre.highlight')
+        if (p) {
+          p.setAttribute('data-line', lines)
+        }
+      }
+    }
+  }
+
+  const Prism = require('prismjs')
   window.Prism = Prism
   require('prismjs/components/prism-clike')
   require('prismjs/components/prism-markup')
@@ -23,7 +38,9 @@
   require('prismjs/components/prism-scala')
   require('prismjs/components/prism-sql')
   require('prismjs/components/prism-swift')
+  require('prismjs/components/prism-velocity')
   require('prismjs/components/prism-yaml')
   require('prismjs/plugins/keep-markup/prism-keep-markup.min')
   require('prismjs/plugins/line-numbers/prism-line-numbers.min')
+  require('prismjs/plugins/line-highlight/prism-line-highlight.min')
 })()
