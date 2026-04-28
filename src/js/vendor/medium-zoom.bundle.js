c26603db68f0da069677624bd085228464878577
Ewan Edwards <eedwards@journaltech.com>
2026-04-28 12:00:28 -0700

    Add support for REST API styling:
    - Add vars for API methods.
    - Add REST API styles using vars.
    - Prevent medium-zoom from zooming API download buttons.

--- src/js/vendor/medium-zoom.bundle.js
+++ src/js/vendor/medium-zoom.bundle.js
@@ -2,7 +2,7 @@
   var Mzm = (window.mzm = require('medium-zoom/dist/medium-zoom.min'))
   Mzm(
     // 'span.frame img, span.zoom img, div.imageblock img',
-    'div.imageblock:not(.nozoom) img, span:not(.nozoom) img',
+    'div.imageblock:not(.nozoom, .download) img, span:not(.nozoom, .download) img',
     {
       background: 'var(--color-white)',
     }
